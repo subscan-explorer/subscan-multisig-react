5398a3f2eab72ce39c5bdbd386f6329945cc75c7
Yang Zhou <deedecho@gmail.com>
2021-11-05 13:29:15 +0800

    feat: add aca multisig

--- src/components/AutoMultiAccounts/index.tsx
+++ src/components/AutoMultiAccounts/index.tsx
@@ -4,34 +4,61 @@ import { encodeAddress } from '@polkadot/util-crypto';
 
 import { useApi } from '../../hooks';
 import { findMultiAccount, updateMultiAccountScope } from '../../utils';
-import { WalletFormValue } from '../../model';
+import { Network, WalletFormValue } from '../../model';
+import { ApiCtx } from '../../providers/api-provider';
+import accounts from './members.json';
+import acaAccounts from './aca_members.json';
 
-import accounts from './accounts.json';
+function addMultiAccount(apiContext: ApiCtx, wallet: WalletFormValue): void {
+  const { networkConfig, api, network } = apiContext;
+  const { name, members, threshold } = wallet;
+  const signatories = members.map(({ address }) => address);
+  const addressPair = members.map(({ address, ...other }) => ({
+    ...other,
+    address: encodeAddress(address, networkConfig.ss58Prefix),
+  }));
+
+  const account = findMultiAccount({
+    threshold,
+    members,
+  });
+
+  if (!account) {
+    keyring.addMultisig(signatories, threshold, {
+      name,
+      addressPair,
+      genesisHash: api?.genesisHash.toHex(),
+    });
+
+    updateMultiAccountScope(wallet as WalletFormValue, network);
+  }
+}
 
 // eslint-disable-next-line react/prop-types
 const AutoMultiAccounts: FC = ({ children }) => {
-  const { networkConfig, api, network } = useApi();
   const [isReady, setIsReady] = useState(false);
+  // const { networkConfig, api, network } = useApi();
+  const apiContext = useApi();
 
   useEffect(() => {
-    const { name, threshold, members } = accounts;
-    const signatories = members.map(({ address }) => address);
-    const addressPair = members.map(({ address, ...other }) => ({
-      ...other,
-      address: encodeAddress(address, networkConfig.ss58Prefix),
-    }));
+    const { name, threshold, share, members, scope } = accounts;
+    addMultiAccount(apiContext, {
+      name,
+      threshold,
+      share,
+      members,
+      scope: scope as Network[],
+      rememberExternal: true,
+    });
 
-    const account = findMultiAccount(accounts);
-
-    if (!account) {
-      keyring.addMultisig(signatories, threshold, {
-        name,
-        addressPair,
-        genesisHash: api?.genesisHash.toHex(),
-      });
-
-      updateMultiAccountScope(accounts as WalletFormValue, network);
-    }
+    addMultiAccount(apiContext, {
+      name: acaAccounts.name,
+      threshold: acaAccounts.threshold,
+      share: acaAccounts.share,
+      members: acaAccounts.members,
+      scope: acaAccounts.scope as Network[],
+      rememberExternal: true,
+    });
 
     setIsReady(true);
 
