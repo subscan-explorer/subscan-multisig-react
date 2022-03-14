46aefaef69e5d060e0062f2d60e9385b18c3518f
vzxh <keqiao.wu@itering.com>
2022-03-14 14:14:10 +0800

    chore: refine call data input dialog UI

--- src/components/TxApprove.tsx
+++ src/components/TxApprove.tsx
@@ -1,7 +1,8 @@
+import { CopyOutlined } from '@ant-design/icons';
 import { Call } from '@polkadot/types/interfaces';
-import { Button, Form, Input, Modal, Popover, Radio, Select, Space } from 'antd';
+import { Button, Form, Input, Modal, Popover, Radio, Select, Space, Typography } from 'antd';
 import { useForm } from 'antd/lib/form/Form';
-import { useCallback, useContext } from 'react';
+import { useCallback, useContext, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { validateMessages } from '../config';
 import i18n from '../config/i18n';
@@ -10,20 +11,24 @@ import { useMultisigContext } from '../hooks/multisigContext';
 import { Entry, TxOperationComponentProps } from '../model';
 import { StatusContext } from '../packages/react-components/src';
 import { PartialQueueTxExtrinsic } from '../packages/react-components/src/Status/types';
-import { makeSure } from '../utils';
+import { makeSure, getThemeVar } from '../utils';
 
 const { info } = Modal;
 
 export function TxApprove({ entry, txSpy, onOperation }: TxOperationComponentProps) {
   const { t } = useTranslation();
   const [form] = useForm();
-  const { accounts, api } = useApi();
+  const { accounts, api, network } = useApi();
   const [getApproveTx] = useMultiApprove();
   const { queueExtrinsic } = useContext(StatusContext);
   const [getUnapprovedInjectedList] = useUnapprovedAccounts();
   const { setIsPageLock, queryInProgress, refreshConfirmedAccount } = useMultisigContext();
   const unapprovedAddresses = getUnapprovedInjectedList(entry);
   const availableAccounts = (accounts ?? []).filter((extAddr) => unapprovedAddresses.includes(extAddr.address));
+  const mainColor = useMemo(() => {
+    return getThemeVar(network, '@project-main-bg');
+  }, [network]);
+
   const handleApprove = useCallback(
     (accountId: string, target: Entry) => {
       setIsPageLock(true);
@@ -74,41 +79,46 @@ export function TxApprove({ entry, txSpy, onOperation }: TxOperationComponentPro
             content: (
               <Form
                 form={form}
-                initialValues={{ account: null, callHash: entry.callHash, callData: '' }}
+                initialValues={{
+                  account: '1ZPVgJpfZ7M2mX6Ta5Vp4byXXq9EQYRrt918VWhSbgkq3Zq',
+                  callData: '',
+                }}
                 layout="vertical"
                 validateMessages={validateMessages[i18n.language as 'en' | 'zh-CN' | 'zh']}
               >
                 <Form.Item label={t('Approve Account')} name="account" rules={[{ required: true }]}>
-                  <Select placeholder={t('Select approve account')}>
-                    {availableAccounts.map((acc) => (
-                      <Select.Option value={acc.address} key={acc.address}>
-                        {acc.meta.name} - {acc.address}
-                      </Select.Option>
-                    ))}
-                  </Select>
+                  {availableAccounts && availableAccounts.length > 0 && (
+                    <Select placeholder={t('Select approve account')}>
+                      {availableAccounts.map((acc) => (
+                        <Select.Option value={acc.address} key={acc.address}>
+                          {acc.meta.name} - {acc.address}
+                        </Select.Option>
+                      ))}
+                    </Select>
+                  )}
                 </Form.Item>
 
-                <Form.Item
-                  label={t('call_hash')}
-                  name="callHash"
-                  validateFirst
-                  rules={[
-                    { required: true },
-                    {
-                      validator: (_, value) => {
-                        try {
-                          api?.registry.createType('CallHash', value);
-                          return Promise.resolve();
-                        } catch (err) {
-                          return Promise.reject();
-                        }
-                      },
-                      message: t('Invalid call hash'),
-                    },
-                  ]}
-                >
-                  <Input placeholder={t('call_hash')} />
-                </Form.Item>
+                <div className="pb-4">
+                  <div className="pl-3">{t('call_hash')}</div>
+                  <Typography.Text
+                    copyable={{
+                      tooltips: false,
+                      text: entry.callHash || '',
+                      icon: (
+                        <CopyOutlined
+                          className="rounded-full opacity-60 cursor-pointer p-1"
+                          style={{
+                            color: mainColor,
+                            backgroundColor: mainColor + '40',
+                          }}
+                          onClick={(e) => e.preventDefault()}
+                        />
+                      ),
+                    }}
+                  >
+                    {entry.callHash}
+                  </Typography.Text>
+                </div>
 
                 <Form.Item
                   label={t('call_data')}
@@ -139,7 +149,7 @@ export function TxApprove({ entry, txSpy, onOperation }: TxOperationComponentPro
             closable: true,
             onOk: () => {
               const account = form.getFieldValue('account');
-              const callHash = form.getFieldValue('callHash');
+              const callHash = entry.callHash;
               const data = form.getFieldValue('callData');
               const callData = api?.registry.createType('Call', data) as Call;
 
