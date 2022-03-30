d7c3944953c18116f31f9e077df56d81f602e77f
vzxh <keqiao.wu@itering.com>
2022-03-30 14:06:46 +0800

    chore: remove unused package.json libraries

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -4,7 +4,6 @@ export * from './donate';
 export * from './down';
 export * from './earth';
 export * from './email';
-export * from './jazzicon';
 export * from './right-circle';
 export * from './swap';
 export * from './swap-crab';
