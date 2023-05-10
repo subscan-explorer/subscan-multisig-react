038dd5e53e7d4ef24ab5088de6e75a4a7a2857ba
WoeOm <1015996366@qq.com>
2023-05-10 13:20:17 +0800

    remove darwinia (#221)
    * feat: remove darwinia chain
    
    * fix: add engines config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@ module.exports = {
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: (_) => ({
-        darwinia: 'linear-gradient(-45deg, #fe3876 0%, #7c30dd 71%, #3a30dd 100%)',
-      }),
       backgroundColor: (_) => ({
-        crab: '#7C30DD',
-        pangolin: '#5745DE',
         polkadot: '#e6007a',
         kusama: '#000',
       }),
@@ -16,15 +11,6 @@ module.exports = {
         lg: '8px',
       },
       colors: (_) => ({
-        pangolin: {
-          main: '#5745DE',
-        },
-        crab: {
-          main: '#7C30DD',
-        },
-        darwinia: {
-          main: '#3a30dd',
-        },
         polkadot: {
           main: '#e6007a',
         },
