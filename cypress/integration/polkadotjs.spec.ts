c208e31c1e4c7da13dc53c303282900e318ff693
WoeOm <1015996366@qq.com>
2024-07-10 15:13:26 +0800

    fix: increase timeout

--- cypress/integration/polkadotjs.spec.ts
+++ cypress/integration/polkadotjs.spec.ts
@@ -1,12 +1,11 @@
 /// <reference types="cypress" />
 
-describe('Wallets display and create', () => {
+describe('Create Multisig Extrinsic', () => {
   before(() => {
-    cy.log(Cypress.config().baseUrl + '/ci/polkadotjs#r%3Dwss%3A%2F%2Fkusama-rpc.dwellir.com');
     cy.visit(Cypress.config().baseUrl + '/ci/polkadotjs#r%3Dwss%3A%2F%2Fkusama-rpc.dwellir.com');
   });
 
-  it('should display create wallets button', () => {
-    cy.get('.polkadotjs', { timeout: 60 * 1000 }).should('be.visible');
+  it('should display success div', () => {
+    cy.get('.polkadotjs', { timeout: 120 * 1000 }).should('be.visible');
   });
 });
